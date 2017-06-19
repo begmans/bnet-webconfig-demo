13609ce4b762fc73515a14826bae3e65a6963c80
Bruno Macherel <BMacherel@telemsa.com>
2017-06-19 18:39:47 +0200

    feat(dragula): Add drag/drop feature

--- react-demo/demo-app/src/List.js
+++ react-demo/demo-app/src/List.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import Dragula from 'react-dragula';
 import './List.css';
 
 class List extends Component {
@@ -7,12 +8,27 @@ class List extends Component {
 		return (
 			<div className="container">
 				<h3 className="container-header">{this.props.header}</h3>
-				<ul className="list">
+				<ul className="list" data-bag="bagOne" ref={this.dragulaDecorator}>
 					{items}
 				</ul>
 			</div>
 		);
 	}
+
+  	dragulaDecorator = (componentBackingInstance) => {
+    	if (componentBackingInstance) {
+      		const options = { mirrorContainer: document.body };
+			const bag = componentBackingInstance.getAttribute('data-bag');
+			if(bag !== null) {
+				Dragula.drakes = Dragula.drakes || {};
+				const drake = Dragula.drakes[bag] || Dragula([], options);
+				drake.containers.push(componentBackingInstance);
+				Dragula.drakes[bag] = drake;
+			} else {
+				Dragula([componentBackingInstance], options);
+			}
+    	}
+  	}
 }
 
 export default List;
